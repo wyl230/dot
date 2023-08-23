f9c8a86286b22df959a2337a2ff9cbdf421139a0
wyl230 <1900012779@pku.edu.cn>
2023-08-23 16:00:52 +0800

    add neoorg && map <leader>cF to format && open project utils

---
+++ lua/plugins/neorg.lua
@@ -0,0 +1,21 @@
+return {
+  "nvim-neorg/neorg",
+  config = function()
+    require('neorg').setup {
+      load = {
+        ["core.defaults"] = {},          -- Loads default behaviour
+        ["core.concealer"] = {},         -- Adds pretty icons to your documents
+        ["core.dirman"] = {              -- Manages Neorg workspaces
+          config = {
+            workspaces = {
+              notes = "~/notes",
+            },
+          },
+        },
+      },
+    }
+  end,
+  run = ":Neorg sync-parsers",
+  requires = "nvim-lua/plenary.nvim",
+  -- lazy = true
+}
