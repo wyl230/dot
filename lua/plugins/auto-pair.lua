d6617546367d27b540daca7e3b2f35c22218660d
wyl230 <1900012779@pku.edu.cn>
2023-08-29 02:14:37 +0800

    change minipair to nvim-autopair

---
+++ lua/plugins/auto-pair.lua
@@ -0,0 +1,12 @@
+return {
+
+  {
+    'wind-wp/nvim-autopairs',
+    event = "InsertEnter",
+    -- opt = {}
+    config = function()
+      require('nvim-autopairs').setup()
+    end
+  },
+  { 'echasnovski/mini.pairs', enabled = false },
+}
