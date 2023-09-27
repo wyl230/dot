c9166ffa98cfe433ed08afd752d6171a39f7e112
wyl <wyl@wyl>
2023-09-27 21:34:33 +0800

    format nvim-ufo

--- lua/plugins/nvim-ufo.lua
+++ lua/plugins/nvim-ufo.lua
@@ -1,55 +1,55 @@
 local handler = function(virtText, lnum, endLnum, width, truncate)
-    local newVirtText = {}
-    local suffix = (' 󰁂 %d '):format(endLnum - lnum)
-    local sufWidth = vim.fn.strdisplaywidth(suffix)
-    local targetWidth = width - sufWidth
-    local curWidth = 0
-    for _, chunk in ipairs(virtText) do
-        local chunkText = chunk[1]
-        local chunkWidth = vim.fn.strdisplaywidth(chunkText)
-        if targetWidth > curWidth + chunkWidth then
-            table.insert(newVirtText, chunk)
-        else
-            chunkText = truncate(chunkText, targetWidth - curWidth)
-            local hlGroup = chunk[2]
-            table.insert(newVirtText, {chunkText, hlGroup})
-            chunkWidth = vim.fn.strdisplaywidth(chunkText)
-            -- str width returned from truncate() may less than 2nd argument, need padding
-            if curWidth + chunkWidth < targetWidth then
-                suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
-            end
-            break
-        end
-        curWidth = curWidth + chunkWidth
+  local newVirtText = {}
+  local suffix = (" 󰁂 %d "):format(endLnum - lnum)
+  local sufWidth = vim.fn.strdisplaywidth(suffix)
+  local targetWidth = width - sufWidth
+  local curWidth = 0
+  for _, chunk in ipairs(virtText) do
+    local chunkText = chunk[1]
+    local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+    if targetWidth > curWidth + chunkWidth then
+      table.insert(newVirtText, chunk)
+    else
+      chunkText = truncate(chunkText, targetWidth - curWidth)
+      local hlGroup = chunk[2]
+      table.insert(newVirtText, { chunkText, hlGroup })
+      chunkWidth = vim.fn.strdisplaywidth(chunkText)
+      -- str width returned from truncate() may less than 2nd argument, need padding
+      if curWidth + chunkWidth < targetWidth then
+        suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
+      end
+      break
     end
-    table.insert(newVirtText, {suffix, 'MoreMsg'})
-    return newVirtText
+    curWidth = curWidth + chunkWidth
+  end
+  table.insert(newVirtText, { suffix, "MoreMsg" })
+  return newVirtText
 end
 
-
 local M = {
-  'kevinhwang91/nvim-ufo',
-  dependencies = { 'kevinhwang91/promise-async' },
+  "kevinhwang91/nvim-ufo",
+  dependencies = { "kevinhwang91/promise-async" },
   opts = {
-    filetype_exclude = { 'help', 'alpha', 'dashboard', 'neo-tree', 'Trouble', 'lazy', 'mason' },
-    fold_virt_text_handler = handler
+    filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "mason" },
+    fold_virt_text_handler = handler,
   },
   config = function(_, opts)
-    vim.api.nvim_create_autocmd('FileType', {
-      group = vim.api.nvim_create_augroup('local_detach_ufo', { clear = true }),
+    vim.api.nvim_create_autocmd("FileType", {
+      group = vim.api.nvim_create_augroup("local_detach_ufo", { clear = true }),
       pattern = opts.filetype_exclude,
       callback = function()
-        require('ufo').detach()
+        require("ufo").detach()
       end,
     })
 
     vim.opt.foldlevelstart = 99
-    require('ufo').setup(opts)
+    require("ufo").setup(opts)
   end,
+  event = "VeryLazy",
 }
 
-vim.o.fillchars = [[eob: ,fold: ,foldopen:↓,foldsep:=,foldclose:>]]
--- vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
-vim.o.foldcolumn = '1'
+-- vim.o.fillchars = [[eob: ,fold: ,foldopen:↓,foldsep:=,foldclose:>]]
+vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+vim.o.foldcolumn = "1"
 
 return M
